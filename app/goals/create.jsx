7a629413eced6e51d44a8de4be07ea87cab592f3
FrankieBayaton <alexiscantancio05@gmail.com>
2025-08-26 08:49:55 +0800

    index and create were changed

--- app/goals/create.jsx
+++ app/goals/create.jsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: 18,
-    backgroundColor: '#21cc8d',
+    backgroundColor: '#cf90e7ff',
     color: 'white',
     borderRadius: 8,
   }
